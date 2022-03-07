e5abb2c5fcc75ace975517a4d5735906ff1d61ec
zixinZoe <46897780+zixinZoe@users.noreply.github.com>
2022-03-07 12:42:51 -0500

    Update best_solver.py

--- best_solver.py
+++ best_solver.py
@@ -1,4 +1,6 @@
 #select the best anchor combination and return only one tagLoc with best anchor combo
+#select the best anchor combination and run 100 times with different Gaussian noise
+#return 100 tagLocs with best anchor combos
 import re
 import sys
 import numpy as np
@@ -44,6 +46,7 @@ def calc_combo(combos,locs,D_complete,estX,estY,cnt,anchor_combo,tag_candidates,
                 estimation = [estX,estY]
                 # estimation = [1000,1000]
                 # print('init_coor: ',init_coor)
+                
                 tag_candidates = np.append(tag_candidates, tag_solver(estimation,[DDoA,locs,zero_coor]),axis =1)
                 # print('tag_can2: ',tag_candidates)
                 RESP_LIST = [0]
@@ -54,13 +57,13 @@ def calc_combo(combos,locs,D_complete,estX,estY,cnt,anchor_combo,tag_candidates,
     return [dop_current,tag_candidates]
 
 #input format: “x0,y0;x1,y1;x2,y2;x4,y4@TDoA01,TDoA02,TDoA04”
-#“x0,y0;x1,y1;x2,y2;x4,y4@TDoA01,TDoA02,TDoA04@roomwidth*roomheight@seed_choice@dop” 
+#“x0,y0;x1,y1;x2,y2;x4,y4@TDoA01,TDoA02,TDoA04@roomwidth*roomheight@seed_choice@dop@sigma@count” 
 def NSDI_read_TDoA_new(line):
     if '@' in line:
-        anchor_combo = []
-        dop_current = []
+        # anchor_combo = []
+        # dop_current = []
 
-        tag_candidates = np.empty((2,0))
+        # tag_candidates = np.empty((2,0))
 
         parts = line.split("@")
         # print('parts0: ',parts[0])
@@ -78,14 +81,31 @@ def NSDI_read_TDoA_new(line):
         init_coor = locs[0,:]
         resp_coor = locs[1:,:]
         read_tdoas = parts[1].split(";")
-        print("read_tdoas: ",read_tdoas)
-        tdoas = []
+        # print("read_tdoas: ",read_tdoas)
+        # tdoas = []
 
+        # mu = 0
+        # sigma = int(parts[5])
+        # print('sigma: ',sigma)
+        # for read_tdoa in read_tdoas:
+        #     noise = np.random.normal(mu, sigma)
+        #     tdoa = int(float(read_tdoa))+noise
+        #     tdoas.append(tdoa)
+        tdoas = []
+        mu = 0
+        sigma = int(parts[5])
+        count = int(parts[6])
+        # print('sigma: ',sigma)
         for read_tdoa in read_tdoas:
-            tdoa = int(float(read_tdoa))
-            tdoas.append(tdoa)
-    # print("tdoas: ",tdoas)
-    # print("resp_coor",resp_coor)
+            noise_tdoa = []
+            noise = np.random.normal(mu, sigma,count)
+            for i in range(count):
+                tdoa = int(float(read_tdoa))
+                noise_tdoa.append(tdoa+noise[i])
+            tdoas.append(noise_tdoa)
+        tdoas = np.transpose(np.array(tdoas) )#100*respnum
+        # print("tdoas: ",tdoas)
+        # print("resp_coor",resp_coor)
         room = parts[2].split("*")
         width = int(room[0])
         height = int(room[1])
@@ -101,38 +121,58 @@ def NSDI_read_TDoA_new(line):
             estX = int(est_coors[0])
             estY = int(est_coors[1])
 
-        if all(tdoa<100000 and tdoa>-100000 for tdoa in tdoas):#tdoa threshold set to be length of the largest diagonal of this building
+        tag_candidates = []
+        # print('typecandidat: ',type(tag_candidates))
+        for row in tdoas:
+            if all(tdoa<100000 and tdoa>-100000 for tdoa in row):#tdoa threshold set to be length of the largest diagonal of this building
 
-            try:
-                D_complete = np.zeros((len(locs[:,0]),len(locs[:,0])))
-                for resp_index in list(range(1,len(resp_coor[:,0])+1)): #resp_index with respect to input string of coors
-                    D_complete[0,resp_index] = tdoas[resp_index-1]
+                try:
+                    # print("in try")
+                    # print("row: ",row)
+                    D_complete = np.zeros((len(locs[:,0]),len(locs[:,0])))
+                    for resp_index in list(range(1,len(resp_coor[:,0])+1)): #resp_index with respect to input string of coors
+                        D_complete[0,resp_index] = row[resp_index-1]
 
-                # print("d_complete: ",D_complete)
+                    # print("d_complete: ",D_complete)
 
-                tag_candidates = np.empty((2,0))
-                cnt =0
-                combos = [] #list of combos of different sizes
-                combos = create_combos(resp_coor,combos)
-                result = calc_combo(combos,locs,D_complete,estX,estY,cnt,anchor_combo,tag_candidates,dop_current)
-                dop_current = result[0]
-                tag_candidates = result[1]
-                print(tag_candidates)
-                min_dop_idx = np.where(dop_current == np.amin(dop_current))[0][0]
-                tagLoc = tag_candidates[:,min_dop_idx]
-                print("tagLocsolver: ",tagLoc)
-                tagLoc = np.reshape(np.array(tagLoc),((1,2)))
-                # serialized = json.dump(tagLoc)
-                return tagLoc
-            except:
-                return np.empty([1,1])
-        else:
-            print("tdoa too large")
-            return np.empty([1,1])
+                    tag_cand = np.empty((2,0))
+                    cnt =0
+                    anchor_combo = []
+                    dop_current = []
+                    combos = [] #list of combos of different sizes
+                    combos = create_combos(resp_coor,combos)
+                    result = calc_combo(combos,locs,D_complete,estX,estY,cnt,anchor_combo,tag_cand,dop_current)
+                    # print("result: ",result)
+                    dop_current = result[0]
+                    tag_cand = result[1]
+                    # print("tagcand: ", tag_cand)
+                    min_dop_idx = np.where(dop_current == np.amin(dop_current))[0][0]
+                    tagLoc = tag_cand[:,min_dop_idx]
+                    # print("tagLocsolver: ",tagLoc)
+                    tagLoc = np.reshape(np.array(tagLoc),((1,2)))
+                    # serialized = json.dump(tagLoc)
+                    # return tagLoc
+                    # print('typecandidates: ',type(tag_candidates))
+                    # print("tagloc: ",tagLoc[0].tolist())
+                    tag_candidates.append(tagLoc[0].tolist())
+                    # print("tag_candidates: ",tag_candidates)
+                    # return tag_candidates
+                except:
+                    # return np.empty([1,1])
+                    # print('typecan: ',type(tag_candidates))
+                    tag_candidates.append([1])
+            else:
+                print("tdoa too large")
+                # return np.empty([1,1])
+                tag_candidates.append([1])
+        # print('tag_candidates: ',tag_candidates)
+        tag_candidates = np.array(tag_candidates)
+        # print('shape: ',tag_candidates.shape)
+        return tag_candidates
     else:
         print('not enough signal captured')
         return np.empty([1,1])
 
-# NSDI_read_TDoA_new("0,0;3600,0;3600,3100;0,3100@115;484;238")
+# NSDI_read_TDoA_new("0,0;3600,0;3600,3100;0,3100@115;484;238@800*800@0,0@best@100")
 # NSDI_read_TDoA_new("0,0;3600,0;3600,3100;0,3100;1500,1500;1000,1500@115;484;238;345;34")
 
